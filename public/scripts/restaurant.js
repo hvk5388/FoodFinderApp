a84834230ccb7c0a489bf79c7aadb96302e79e9b
Taylor-Bracone <tdbracone@gmail.com>
2020-11-17 03:29:10 -0500

    added favorite routing- own api and zipcode api

--- public/scripts/restaurant.js
+++ public/scripts/restaurant.js
@@ -1,5 +1,7 @@
 var latitude = sessionStorage.getItem("latitude");
 var longitude = sessionStorage.getItem("longitude");
+var restName = "restuarantname"; /* example name */
+var restID = 12; /* example ID */
 
 $(document).ready(function () {
    
@@ -14,7 +16,12 @@ $(document).ready(function () {
                }).addTo(mymap);
             
                L.marker([latitude, longitude]).addTo(mymap)
-                  .bindPopup("<h4>Current Location</h4>").openPopup();
+                  .bindPopup("<h4>Current Location</h4><form action='http://localhost:3000/favorite' method='POST'>" +
+                  "<input type='hidden' name='id' value=" + restID + ">" +
+                  "<input type='hidden' name='latitude' value=" + latitude + ">" +
+                  "<input type='hidden' name='longitude' value=" + longitude + ">" +
+                  "<input type='hidden' name='restuarant' value=" + restName + ">" +
+                  "<button type='submit' class='favoriteBtn'>Favorite</button></form>").openPopup();
             
                L.circle([latitude, longitude], 1000, {
                   color: 'red',
@@ -38,7 +45,25 @@ $(document).ready(function () {
                      .openOn(mymap);
                }
             
-               mymap.on('click', onMapClick);
-               
+               mymap.on('click', onMapClick);           
 });
+
+function getRestaurant(){
+   var clientKey = 'afa7fba6d4bd0a50844c37bbac688903';
+   var zipcode = document.getElementById('zipcodeInput').value;
+   var rootURL = 'https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/' + clientKey + '/info.json/' + zipcode + '/degrees';
+
+   $.ajax({
+     url: rootURL,
+     method: `GET`,
+     dataType: "json",
+     headers: { "User-key": clientKey },
+     success: successRestaurant
+  });
+}
+
+/*the function is accessing the objects attributes*/
+function successRestaurant(data) {
+   /* Add for loop for locations */
+}
                
